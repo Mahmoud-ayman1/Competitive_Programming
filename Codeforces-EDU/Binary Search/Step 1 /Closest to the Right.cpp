b96065ebbc7bd14b59842ca785ee17527535e50a
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2023-06-07 11:13:29 +0300

    Create Closest to the Right.cpp

---
+++ Codeforces-EDU/Binary Search/Step 1 /Closest to the Right.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n,k;cin>>n>>k;
+    int s[n+1];
+    for(int i=1;i<=n;i++)cin>>s[i];
+    while(k--)
+    {
+        int x;cin>>x;
+        int l=1,r=n,ans=n+1;
+        while(l<=r)
+        {
+            int mid=l+r>>1;
+            if(s[mid]>=x)ans=mid,r=mid-1;
+            else l=mid+1;
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
