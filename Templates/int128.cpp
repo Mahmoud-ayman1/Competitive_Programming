2b9aef404021383e775f83d46dcbddc7597853ab
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2023-08-06 16:47:07 +0300

    Create int128.cpp

---
+++ Templates/int128.cpp
@@ -0,0 +1,22 @@
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+bool cmp(__int128 x, __int128 y) { return x > y; }
