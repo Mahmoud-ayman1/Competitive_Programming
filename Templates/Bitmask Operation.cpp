d6180d451a978f872d99832dd0128919dec2ee1d
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2023-09-20 12:34:56 +0300

    Update Bitmask Operation.cpp

--- Templates/Bitmask Operation.cpp
+++ Templates/Bitmask Operation.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
 bool Knowbit(ll n,int i)
 {
     return (n>>i)&1;
@@ -18,4 +21,27 @@ bool isPowerOfTwo(int n)
 {
     if(n==0)return 0;
     return !(n&(n-1));
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n;cin>>n;
+    int s[n];
+    for(int i=0;i<n;i++) cin>>s[i];
+    int ans=0;
+    for(int mask=0;mask<(1<<n);mask++)
+    {
+        int even=0,odd=0;
+        for(int i=0;i<n;i++)
+        {
+            if(Knowbit(mask,i)==1)
+            {
+                if(s[i]&1) odd++;
+                else even++;
+            }
+        }
+        if(even>odd) ans++;
+    }
+    cout<<ans;
+    return 0;
 }
