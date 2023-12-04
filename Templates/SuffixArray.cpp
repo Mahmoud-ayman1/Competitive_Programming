5f4dd8b445d02a9c9b1f564116997bb0f2a1186c
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2023-12-04 09:46:29 +0200

    Update SuffixArray.cpp

--- Templates/SuffixArray.cpp
+++ Templates/SuffixArray.cpp
@@ -1,3 +1,19 @@
+void countSort(vector<int>&p,vector<int>&c)
+{
+    int n=p.size();
+    vector<int>cnt(n);
+    for(auto it:c) cnt[it]++;
+    vector<int>p_new(n),pos(n);
+    pos[0]=0;
+    for(int i=1;i<n;i++) pos[i]=pos[i-1]+cnt[i-1];
+    for(auto it:p)
+    {
+        int i=c[it];
+        p_new[pos[i]]=it;
+        pos[i]++;
+    }
+    p=p_new;
+}
 vector<int>SuffixArray(string &s)
 {
     s+='$';
@@ -16,16 +32,18 @@ vector<int>SuffixArray(string &s)
     int k=0;
     while((1<<k)<n)
     {
-        vector<pair<pair<int,int>,int>>su(n);
-        for(int i=0;i<n;i++) su[i]={{c[i],c[(i+(1<<k))%n]},i};
-        sort(su.begin(),su.end());
-        for(int i=0;i<n;i++) p[i]=su[i].second;
-        c[p[0]]=0;
+        for(int i=0;i<n;i++) p[i]=(p[i]-(1<<k)+n)%n;
+        countSort(p,c);
+        vector<int>c_new(n);
+        c_new[p[0]]=0;
         for(int i=1;i<n;i++)
         {
-            if(su[i].first==su[i-1].first) c[p[i]]=c[p[i-1]];
-            else c[p[i]]=c[p[i-1]]+1;
+            pair<int,int>last={c[p[i-1]],c[(p[i-1]+(1<<k))%n]};
+            pair<int,int>cur={c[p[i]],c[(p[i]+(1<<k))%n]};
+            if(cur==last) c_new[p[i]]=c_new[p[i-1]];
+            else c_new[p[i]]=c_new[p[i-1]]+1;
         }
+        c=c_new;
         k++;
     }
     return p;
