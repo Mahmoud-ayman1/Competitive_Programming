9ebbb3c47231c82e9fcebc9f8e373d82c1ac4c6d
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-05-07 13:25:26 +0300

    Create A - Segment with Small Sum.cpp

---
+++ Codeforces-EDU/2-pointers/A - Segment with Small Sum.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n,target;cin>>n>>target;
+    ll s[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>s[i];
+    }
+    ll l=0,r=0,sum=0,ans=0;
+    while(l<n)
+    {
+        while(r<n&&sum+s[r]<=target)
+        {
+            sum+=s[r];
+            r++;
+        }
+        ans=max(ans,r-l);
+        sum-=s[l];
+        l++;
+    }
+    cout<<ans;
+    return 0;
+}
