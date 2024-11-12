98925144e82f8ac113a3125b7ea833f2c0fdab79
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-11-12 14:25:53 +0200

    Update phi.cpp

--- Templates/Math and Number Theory/phi.cpp
+++ Templates/Math and Number Theory/phi.cpp
@@ -17,3 +17,19 @@ void sieve()
         }
     }
 }
+
+
+ll phi(ll n)
+{
+    ll ret=n;
+    for(ll i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            while(n%i==0)n/=i;
+            ret-=ret/i;
+        }
+    }
+    if(n>1) ret-=ret/n;
+    return ret;
+}
