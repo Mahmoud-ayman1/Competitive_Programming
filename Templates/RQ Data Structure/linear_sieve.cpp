4f93860e606ce8780fbc17e06e8379db095460a7
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-11-12 14:25:03 +0200

    Create linear_sieve.cpp

---
+++ Templates/RQ Data Structure/linear_sieve.cpp
@@ -0,0 +1,16 @@
+const int N=1e5+5;
+vector<bool>isPrime(N,true);vector<int>p;
+void linear_sieve()
+{
+    isPrime[0]=isPrime[1]=0;
+    for(ll i=2;i<N;i++)
+    {
+        if(isPrime[i]==1) p.push_back(i);
+        for(ll o=0;o<p.size();o++)
+        {
+            if(i*p[o]>=N) break;
+            isPrime[i*p[o]]=0;
+            if(i%p[o]==0) break;
+        }
+    }
+}
