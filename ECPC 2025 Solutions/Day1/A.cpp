f79c48478f78da6f2250f493a38fe150c777980b
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2025-07-31 10:12:52 +0300

    Create A.cpp

---
+++ ECPC 2025 Solutions/Day1/A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll a,b,c;cin>>a>>b>>c;
+    cout<<a-b-c;
+    return 0;
+}
